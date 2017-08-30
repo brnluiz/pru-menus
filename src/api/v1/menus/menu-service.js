183244aee118c1ffd1f6cd84e89fbba4b984d78a
Bruno Luiz <contact@brunoluiz.net>
2017-08-30 08:21:07 -0300

    Fix menu-service behavior

--- src/api/v1/menus/menu-service.js
+++ src/api/v1/menus/menu-service.js
@@ -1,10 +1,20 @@
+const mongoose = require('mongoose')
 const error = require('../../../errors')
 const locationRepository = require('../../../db/repositories/location-repository')
 const menuRepository = require('../../../db/repositories/menu-repository')
 
 module.exports = {
   async get (id) {
-    return menuRepository.get(id)
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new error.ValidationError('Not a valid id')
+    }
+
+    const menu = await menuRepository.get(id)
+    if (!menu) {
+      throw new error.NotFoundError('Menu not found')
+    }
+
+    return menu
   },
   async getByLocation (locationSlug, startDate, endDate) {
     const location = await locationRepository.get(locationSlug)
@@ -13,6 +23,10 @@ module.exports = {
     }
 
     const menus = await menuRepository.getByLocation(location._id, startDate, endDate)
+    if (!menus.length) {
+      throw new error.NotFoundError('Menus not found')
+    }
+
     return menus
   },
   async create (menuIn) {
