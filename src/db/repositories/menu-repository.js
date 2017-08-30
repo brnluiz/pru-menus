d618384140abadf97641501ca4e7d56644178e52
Bruno Luiz <contact@brunoluiz.net>
2017-08-30 00:32:12 -0300

    Improves menu-repository

--- src/db/repositories/menu-repository.js
+++ src/db/repositories/menu-repository.js
@@ -2,20 +2,18 @@ const Menu = require('../models/menu')
 
 module.exports = {
   get (id) {
-    return Menu.findOne({ id })
+    return Menu.findOne({ id }).lean()
   },
   getByLocation (locationId, startDate, endDate) {
-    return Menu.find({
-      locationId,
-      date: {
-        $gte: startDate,
-        $lt: endDate
-      }
-    })
+    const query = { locationId }
+
+    if (startDate) query.date['$gte'] = startDate
+    if (endDate) query.date['$lt'] = endDate
+
+    return Menu.find(query).lean()
   },
   create (menuIn) {
-    const menu = new Menu(menuIn)
-    return menu.save()
+    return Menu.create(menuIn)
   },
   createBulk (menusIn) {
     return Menu.insertMany(menusIn)
