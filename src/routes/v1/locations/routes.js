c852c812e532b641ed506de0da4d921b27f79f34
Bruno Luiz <contact@brunoluiz.net>
2017-10-02 00:45:58 -0300

    Update locations apidoc

--- src/routes/v1/locations/routes.js
+++ src/routes/v1/locations/routes.js
@@ -6,15 +6,45 @@ const collectPostByLocationController = require('./controllers/locations-collect
 
 const locationMiddleware = require('./middlewares/location-middleware')
 
+/**
+ * @api {post} /locations Create Location
+ * @apiGroup Locations
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess Success
+ * @apiUse InternalServerError
+ * @apiUse LocationSchema
+ * @apiUse LocationExample
+ */
 router.post('/locations',
   locationMiddleware,
   locationPostController
 )
 
+/**
+ * @api {post} /locations/collect Scrap menus (all locations)
+ * @apiGroup Locations
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess Success
+ * @apiUse InternalServerError
+ * @apiUse NotFoundError
+ * @apiUse ValidationError
+ */
 router.post('/locations/collect',
   collectPostController
 )
 
+/**
+ * @api {post} /locations/:locationId/collect Scrap menu
+ * @apiGroup Locations
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess Success
+ * @apiUse InternalServerError
+ * @apiUse NotFoundError
+ * @apiUse ValidationError
+ */
 router.post('/locations/:locationSlug/collect',
   collectPostByLocationController
 )
